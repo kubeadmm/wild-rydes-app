e2f49b23eea27ebf73afe5a4905f0ad0d639adef
Rahul Dhaka P <92939710+kubeadmm@users.noreply.github.com>
2025-04-08 20:03:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "Company: Wild Rydes | Developer: Rahul Dhaka p | ID: 100953252"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
